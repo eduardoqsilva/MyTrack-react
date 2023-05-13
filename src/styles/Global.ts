934fd5945def2902010cc14675bb8a21b7b53e42
eduardoqsilva <eduadsilva@outlook.com>
2023-05-12 22:47:06 -0300

    first commit :tada:

---
+++ src/styles/Global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+import { colors } from "./variables";
+
+export const GlobalStyled = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${colors.gray1};
+
+    font-family: 'Roboto', sans-serif;
+    color: ${colors.gray7}
+  }
+`
---
+++ src/styles/variables/index.ts
@@ -0,0 +1,25 @@
+export const font = {
+  default: 'Roboto, sans-serif',
+} as const
+
+export const colors = {
+  blu: '#1DA1F3',
+  blu_dark: '#13669A',
+  blu_light: '#72C9FF',
+
+  red: '#F03847',
+  red_dark: '#7A1921',
+  
+  green_light: '#8FF7A7',
+
+
+  gray1: '#121214',
+  gray2: '#202024',
+  gray3: '#323238',
+  gray4: '#7C7C8A',
+  gray5: '#8D8D99',
+  gray6: '#C4C4CC',
+  gray7: '#E1E1E6',
+
+  white: '#FFFF',
+} as const
