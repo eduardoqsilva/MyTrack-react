7994b9860a78217f7a6937cac84e256a11ef146f
eduardoqsilva <eduadsilva@outlook.com>
2023-05-16 22:57:02 -0300

    update home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,26 @@
+import { useEffect } from "react";
+import { Loading } from "../../components/Loading";
 import { TimeLine } from "../../components/TimeLine";
+import { UseFetching } from "../../services/api/useFetch";
+import { ContainerHome } from "./home.styled";
+import { useParams } from "react-router-dom";
 
 export function Home() {
+
+  const num = useParams()
+  const {data, isFetching} = UseFetching(num.cod)
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
+
   return (
-    <TimeLine />
+    <>
+      <Loading load={isFetching}/>
+      <ContainerHome>
+        <TimeLine events={data?.data.events}/>
+      </ContainerHome>
+    </>
   )
 }
