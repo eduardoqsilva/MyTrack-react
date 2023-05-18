00368cc46d63db605776ae3b1d98f11d3df5f0cb
eduardoqsilva <eduadsilva@outlook.com>
2023-05-17 23:21:33 -0300

    change lottie animation and add a localStorage function

--- src/components/Saveds/index.tsx
+++ src/components/Saveds/index.tsx
@@ -1,26 +1,43 @@
+import { useEffect, useState } from "react";
 import { Card } from "../Cards";
 import { TextWrapper, WrapperSavedsContant } from "./saveds.styled";
+import { Player } from "@lottiefiles/react-lottie-player";
+import empty from '../../assets/lotties/empty-file.json'
+
+interface Item {
+  category: "tecnologia" | "comida" | "roupas" | "geral";
+  title: string;
+  cod: string;
+}
 
 export function Saveds() {
+
+  const [data, setData] = useState<Item[]>([])
+
+  useEffect(() => {
+    const valueText = localStorage.getItem('saved')
+
+    if(valueText) {
+      setData(JSON.parse(valueText))
+    }
+
+  }, [])
+
   return (
     <WrapperSavedsContant>
-      <TextWrapper><span>Encomendas salvas:</span> <span>4</span></TextWrapper>
-      <Card 
-        tag="tecnologia"
-        text="Notebook samsung i5"
-      />
-      <Card 
-        tag="roupas"
-        text="Roupas shein casacos"
-      />
-      <Card 
-        tag="comida"
-        text="Chocolates cacau show"
-      />
-      <Card 
-        tag="geral"
-        text="Adesivos para o carro"
-      />
+      <TextWrapper><span>Encomendas salvas:</span> <span>{data.length}</span></TextWrapper>
+      {data.length > 0 ? data.map((item, index) => {
+        return (
+          <Card 
+            key={index}
+            tag={item.category}
+            text={item.title}
+            cod={item.cod}
+            index={index}
+            data={[data, setData]}
+          />
+        )
+      }) : <Player src={empty} autoplay loop/>}
     </WrapperSavedsContant>
   )
 }
