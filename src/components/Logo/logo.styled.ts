1c4bdcd12f6cbedcb4547e61f557a5f1e9d8b0be
eduardoqsilva <eduadsilva@outlook.com>
2023-05-17 23:34:33 -0300

    add mobile responsive

--- src/components/Logo/logo.styled.ts
+++ src/components/Logo/logo.styled.ts
@@ -4,4 +4,9 @@ export const LogoWrapper = styled.div`
   position: absolute;
   left: 1.5rem;
   top: 1rem;
+
+  @media (max-width: 700px) {
+    left: 50%;
+    transform: translateX(-50%);
+  }
 `
