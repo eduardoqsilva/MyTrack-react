dcb4260d96016e386099e584ed9797868a1ca78f
eduardoqsilva <eduadsilva@outlook.com>
2023-06-03 22:03:52 -0300

    update routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "../pages/Home";
-import { Tracks } from "../pages/Tracks";
 import { Default } from "../layouts/default";
+import { Home } from "../pages/Home";
+import { Track } from "../pages/Track";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Default />}>
-        <Route path={'/'} element={<Tracks />} />
-        <Route path={'/home/:cod'} element={<Home />} />
+        <Route path={'/'} element={<Home />} />
+        <Route path={'/track/:cod'} element={<Track />} />
         {/* <Route path={'*'} element={} /> */}
       </Route>
     </Routes>
