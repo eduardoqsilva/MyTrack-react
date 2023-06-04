dc1025e0900b5f7eb45f46ff7b81eb7ab565e05b
eduardoqsilva <eduadsilva@outlook.com>
2023-06-03 22:13:09 -0300

    update global styles

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -20,4 +20,10 @@ export const GlobalStyled = createGlobalStyle`
   button {
     cursor: pointer;
   }
+
+  a {
+    text-decoration: none;
+    font-weight: 600;
+    color: ${colors.blu_dark}
+  }
 `
