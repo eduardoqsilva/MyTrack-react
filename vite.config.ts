ba217c3aefe3918f6326c5f25ba6abbfdb7fe5e3
eduardoqsilva <eduadsilva@outlook.com>
2023-09-21 18:28:15 -0300

    add a pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa"
+
+
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType:'prompt',
+  includeAssets:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"My Tracks",
+    short_name:"MyTracks",
+    description:"Rastreie sua encomenda",
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'/android-chrome-512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#121214',
+  background_color:'#121214',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
 })
